b2ab40e25b836c453eec402be1f9d3b9ce69a9e7
ocinpp <ocin25@gmail.com>
2025-02-16 23:24:30 +0800

    Set up tailwindcss-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "gradient-x": "gradient 4s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
